c47ee4a9eaeeafd943198702ca074ca3cf12f959
Aleksandr Boiarinov <aboiarin@c1b3c3.42berlin.de>
2023-10-02 15:11:42 +0200

    ver 0.1.4

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: aboiarin <aboiarin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/28 16:53:41 by aboiarin          #+#    #+#             */
-/*   Updated: 2023/09/29 15:45:51 by aboiarin         ###   ########.fr       */
+/*   Updated: 2023/10/02 15:10:01 by aboiarin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,31 @@
 #include <stdio.h>
 #include "push_swap.h"
 
-void sa(int *a, unsigned int size)
+void    sa(int *a)
 {
-	int	t;
-	
-	t = a[size];
-	a[size] = a[size - 1];
-	a[size - 1] = t;
+    int    t;
+
+    t = a[0];
+    a[0] = a[1];
+    a[1] = t;
 }
 
-void sb(int *b, unsigned int size)
+/*void	sb(int **b, unsigned int size)
 {
 	int	t;
-	
+
 	t = b[size];
 	b[size] = b[size - 1];
 	b[size - 1] = t;
 }
 
-void ss(int *a, int *b, unsigned int size)
+void	ss(int **a, int **b, unsigned int size)
 {
 	sa(&a, size);
 	sb(&b, size);
 }
 
-void pa(int *a, int *b, unsigned int size)
+void	pa(int **a, int **b, unsigned int size)
 {
 	int	t;
 	int	c;
@@ -61,7 +61,7 @@ void pa(int *a, int *b, unsigned int size)
 	}
 }
 
-void pb(int *a, int *b, unsigned int size)
+void	pb(int **a, int **b, unsigned int size)
 {
 	int	t;
 	int	c;
@@ -82,23 +82,21 @@ void pb(int *a, int *b, unsigned int size)
 		b[size] = 0;
 		a[size] = t;
 	}
-}
+}*/
 
-void ra(int *a, unsigned int size)
+void    ra(int *a, unsigned int size)
 {
-	int	t;
+    int    t;
 
-	t = a[size];
-	while (size > 1)
-	{
-		a[size] = a[size - 1];
-		size--;
-	}
-	a[0] = t;
+    t = a[size];
+    while (size > 1)
+    {
+        a[size] = a[size - 1];
+        size--;
+    }
+    a[0] = t;
 }
-
-
-void rb(int *b, unsigned int size)
+/*void	rb(int **b, unsigned int size)
 {
 	int	t;
 
@@ -111,28 +109,28 @@ void rb(int *b, unsigned int size)
 	b[0] = t;
 }
 
-void rr(int *a, int *b, unsigned int size)
+void	rr(int *a, int *b, unsigned int size)
 {
 	ra(&a, size);
 	rb(&b, size);
-}
+}*/
 
-void rra(int *a, unsigned int size)
+void    rra(int *a, unsigned int size)
 {
-	int	t;
-	int	i;
+    int    t;
+    int    i;
 
-	t = a[0];
-	i = 0;
-	while (i <= size - 1)
-	{
-		a[i] = a[i + 1];
-		i++;
-	}
-	a[size] = t;
+    t = a[0];
+    i = 0;
+    while (i <= size - 1)
+    {
+        a[i] = a[i + 1];
+        i++;
+    }
+    a[size] = t;
 }
 
-void rrb(int *b, unsigned int size)
+/*void	rrb(int **b, unsigned int size)
 {
 	int	t;
 	int	i;
@@ -147,37 +145,48 @@ void rrb(int *b, unsigned int size)
 	b[size] = t;
 }
 
-void rrr(int *a, int *b, unsigned int size)
+void	rrr(int *a, int *b, unsigned int size)
 {
 	rra(&a, size);
 	rrb(&b, size);
-}
+}*/
 
-int	main (int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	int	*a;
-	int	*b;
-	int	i;
-	unsigned int size;
-	
+	int				*a;
+//	int				*b;
+	int				i;
+	int				j;
+	unsigned int	size;
+
+	i = 0;
+	j = 1;
 	if (argc > 1)
 	{
-		size = argc;
+		size = argc - 1;
 		a = (int *)malloc(sizeof(int) * size);
-		b = (int *)malloc(sizeof(int) * size);
-		if (a == 0 || b == 0)
+//		b = (int *)malloc(sizeof(int) * size);
+		if (a == 0) //|| b == 0)
 		{
 			printf("Malloc error\n");
 			return (0);
 		}
-		if (ft_atoi(argv[i]) != 0)
+		while (i < size)
 		{
-			a[i] = ft_atoi(argv[i]);
+			a[i] = atoi(argv[j]);
+			i++;
+			j++;
+		}
+		i = 0;
+		if (size == 3)
+			if_three(a, size);
+		while (i < size)
+		{
+			printf("%i ", a[i]);
 			i++;
 		}
-		push_swap(a, b, size);
 		free(a);
-		free(b);
+//		free(b);
 	}
 	return (0);
 }
